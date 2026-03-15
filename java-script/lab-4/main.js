d678bf6b62ec897e1994fbd1a6bf39e6a2e66fc7
Vitalii <vas172674@gmail.com>
2026-03-15 23:18:23 +0200

    lab 4: - finish task 2; - add task 3 (half).

--- java-script/lab-4/main.js
+++ java-script/lab-4/main.js
@@ -15,9 +15,15 @@ console.log(fruits);
 // 4.
 console.log("Apple index: " + fruits.findIndex( (str) => str === "apple")); // 4.
 
-// Task 2. Colors.
+/////////////////////
+// Task 2. Colors. //
+/////////////////////
+
 console.log("Task 2");
-let colors = ["blue", "yellow", "pink"];
+// 1.
+let colors = ["blue", "yellow", "pink", "light blue"];
+
+// 2.
 // const shortestColor = colors.reduce((prev, curr) => prev.length <= curr.length ? prev : curr);
 // const longestColor = colors.reduce((prev, curr) => prev.length >= curr.length ? prev : curr);
 
@@ -30,3 +36,44 @@ const result = colors.reduce((acc, curr) => {
 }, {shortest: colors[0], longest: colors[0]});
 console.log("Shortest color: " + result.shortest);
 console.log("Longest color: " + result.longest);
+
+// 3.
+const blueColors = colors.filter(item => item.includes("blue")); // Case sensitive.
+const blueColors2 = colors.filter(item => item.toLowerCase().includes("blue")) // Case insensitive.
+
+const regex = /blue/i
+const blueColors3 = colors.filter(item => regex.test(item)); 
+
+// 4.
+console.log(blueColors.join(", "));
+
+////////////
+// Task 3 //
+////////////
+class Employee {
+    constructor(name, age, profession) {
+        this.name = name;
+        this.age = age;
+        this.profession = profession;
+    }
+}
+// 1.
+let employees = [
+    new Employee("Bogdan", 19, "Brewer"), 
+    new Employee("Andriy", 20, "Software Engineer")
+];
+console.log(employees);
+
+// 2.
+let sortedEmployees = employees.toSorted((a, b) => a.name.localeCompare(b.name));
+
+let softwareEngineers = employees.filter(obj => obj.profession.includes("Software Engineer"));
+
+const keywords = ["Software Engineer", "Developer", "Programmer"];
+let softwareEngineers2 = employees.filter(obj => 
+    keywords.some(word => obj.profession.includes(word))
+);
+
+console.log(softwareEngineers);
+
+// 3.
