840430d794f5ac9f722b042d8cb308204db9985f
Vitalii <vas172674@gmail.com>
2026-03-22 23:45:23 +0200

    add js lab-4 task-4

--- java-script/lab-4/main.js
+++ java-script/lab-4/main.js
@@ -1,6 +1,9 @@
 "use strict"
 
-// Task 1.
+/////////////
+// Task 1. //
+/////////////
+
 let fruits = ["apple", "banana", "pear", "strawberry", "blueberry"]; 
 console.log(fruits);
 // 1.
@@ -13,7 +16,7 @@ console.log(fruits);
 fruits.reverse(); 
 console.log(fruits);
 // 4.
-console.log("Apple index: " + fruits.findIndex( (str) => str === "apple")); // 4.
+console.log("Apple index: " + fruits.findIndex((str) => str === "apple")); // 4.
 
 /////////////////////
 // Task 2. Colors. //
@@ -60,13 +63,15 @@ class Employee {
 // 1.
 let employees = [
     new Employee("Bogdan", 19, "Brewer"), 
-    new Employee("Andriy", 20, "Software Engineer")
+    new Employee("Andriy", 20, "Software Engineer"),
+    new Employee("Kateryna", 17, "Barista")
 ];
 console.log(employees);
 
 // 2.
 let sortedEmployees = employees.toSorted((a, b) => a.name.localeCompare(b.name));
 
+// 3.
 let softwareEngineers = employees.filter(obj => obj.profession.includes("Software Engineer"));
 
 const keywords = ["Software Engineer", "Developer", "Programmer"];
@@ -76,4 +81,63 @@ let softwareEngineers2 = employees.filter(obj =>
 
 console.log(softwareEngineers);
 
+// 4.
+const index = employees.findIndex(emp => emp.age < 18);
+if (index !== -1) {
+    console.log(employees.at(index));
+    employees.splice(index, 1);
+}
+
+// 5.
+employees.push(new Employee("Vitalii", 18, "Software Engineer"));
+employees.splice(1, 0, new Employee("Olena", 24, "QA"));
+console.log(employees);
+
+/////////////
+// Task 4. //
+/////////////
+
+// 1.
+class Student {
+    constructor(name, age, course) {
+        this.name = name;
+        this.age = age;
+        this.course = course;
+    }
+};
+
+const students = [
+    new Student("Oleksiy", 20, 4),
+    new Student("Bogdan", 17, 1),
+    new Student("Bogdan2", 19, 3)
+];
+
+// 2.
+const updatedStudents = students.filter(student => student.name !== "Oleksiy");
+console.log(updatedStudents);
+
+const studentIndex = students.findIndex(student => student.name === "Oleksiy");
+if (index !== -1) {
+    console.log(students.at(studentIndex));
+    students.splice(studentIndex, 1);
+}
+console.log(students);
+
 // 3.
+students.push(new Student("Vlad", 18, 2));
+
+// 4.
+students.sort((a, b) => a.age - b.age);
+console.log(students);
+
+// 5.
+const thirdDegreeStudent = students.find(student => student.course === 3);
+console.log(thirdDegreeStudent);
+
+/////////////
+// Task 5. //
+/////////////
+
+
+
+
